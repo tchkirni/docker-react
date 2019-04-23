d784be5ebb8870daf81d8796db12f0014233850f
Taofik Chkirni <t.chkirni@gmail.com>
2019-04-23 11:56:16 +0200

    Inital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World <span aria-label="globe" role="img">🌐</span> !
         </p>
         <a
           className="App-link"
